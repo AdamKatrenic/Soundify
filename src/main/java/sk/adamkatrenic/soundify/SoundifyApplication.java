bbf2039d421f87a4d9a680d091bf5ab71f8a3f4e
AdamKatrenic <adamkatrenic@gmail.com>
2026-05-11 20:42:36 +0200

    Initial commit - Soundify project setup

---
+++ src/main/java/sk/adamkatrenic/soundify/SoundifyApplication.java
@@ -0,0 +1,13 @@
+package sk.adamkatrenic.soundify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/adamkatrenic/soundify/SoundifyApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.adamkatrenic.soundify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
