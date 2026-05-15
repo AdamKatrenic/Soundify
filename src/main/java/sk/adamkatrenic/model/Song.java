50111dad745d08aacdfb7987fa2b28b9c4240f8e
AdamKatrenic <adamkatrenic@gmail.com>
2026-05-15 22:33:04 +0200

    CorsConfig and SecurityConfig fix

--- src/main/java/sk/adamkatrenic/model/Song.java
+++ src/main/java/sk/adamkatrenic/model/Song.java
@@ -18,6 +18,9 @@ public class Song {
     @Column
     private int duration;
 
+    @Column
+    private String audioUrl;
+
     @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "album_id")
@@ -25,14 +28,16 @@ public class Song {
 
     public Song() {}
 
-    public Song(String title, int duration, Album album) {
+    public Song(String title, int duration, String audioUrl, Album album) {
         this.title = title;
         this.duration = duration;
+        this.audioUrl = audioUrl;
         this.album = album;
     }
 
     public Long getId() { return id; }
     public String getTitle() { return title; }
     public int getDuration() { return duration; }
+    public String getAudioUrl() { return audioUrl; }
     public Album getAlbum() { return album; }
 }
