e2f44fa9db97f739003512b60db0be6a0e4344b7
AdamKatrenic <adamkatrenic@gmail.com>
2026-05-15 19:17:38 +0200

    commit

--- src/test/java/sk/adamkatrenic/service/UserServiceTest.java
+++ src/test/java/sk/adamkatrenic/service/UserServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import sk.adamkatrenic.model.User;
 import sk.adamkatrenic.repository.UserRepository;
